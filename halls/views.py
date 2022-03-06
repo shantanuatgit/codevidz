58fdb8b8bc269c5ee371d3046b7535a8e269b935
shantanuatgit <shantanu3250@gmail.com>
2022-03-06 12:57:12 +0530

    created models and login-signup view

---
+++ halls/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .models import *
+
+
+def home(request):
+    return render(request, 'halls/home.html')
+
+class Signup(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        view = super(Signup, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
+
+class CreateHall(generic.CreateView):
+    model = Hall
+    fields = ['title']
+    template_name = 'halls/create_hall.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')
